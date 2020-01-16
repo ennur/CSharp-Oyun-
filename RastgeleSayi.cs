1ad5ddf760fed2de28acd3d4e524f4282375f835
ennur <58693641+ennur@users.noreply.github.com>
2020-01-16 17:29:59 +0200

    Create RastgeleSayi.cs

---
+++ RastgeleSayi.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Oyun
+{
+    class RastgeleSayi
+    {
+        public static int SayiUret(int min, int max)
+        {
+            if (rastgele == null)
+                rastgele = new Random();
+
+            return rastgele.Next(min, max);
+        }
+
+        private static Random rastgele;
+    }
+}
